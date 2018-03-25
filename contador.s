ead5985f1b50b3092a909a97f3c328010aa9a866
Hebeu <wesley_ms14@hotmail.com>
2018-03-25 14:10:09 -0300

    Update contador.s

--- contador.s
+++ contador.s
@@ -1,9 +1,9 @@
 #######################################################################################
-#Contador e operação xor
+#Contador
 #######################################################################################			
 
 main:		
-		#Atribuições
+		#AtribuiÃ§Ãµes
 		
 		addi r2,r2,20
 		addi r3,r3,20 # ou mov r3,r2 
@@ -12,14 +12,14 @@ main:
 ########################################################################################
 #Forma 1 de fazer loop e condicoes
 
-repetir:	bne r2, r0, decrementa 		#Laço que se repete até o r2 ser igual a zero  	
+repetir:	bne r2, r0, decrementa 		#LaÃ§o que se repete atÃ© o r2 ser igual a zero  	
 		ret #Finaliza o loop
 		
 decrementa:	subi r2,r2,1
-		call repetir #chama o laço repetir
+		call repetir #chama o laÃ§o repetir
 #########################################################################################			
 #Forma 2
 
-repetir2:	addi r4,r4,1			#Laço que se repete até o r4 ser igual ao r3	
+repetir2:	addi r4,r4,1			#LaÃ§o que se repete atÃ© o r4 ser igual ao r3	
 		bne r4,r3,repetir2 #if(r4!=r3){//continua no loop}	
 		
