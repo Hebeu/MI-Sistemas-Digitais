b7b40ab8c69fda4689cb8fdfb10b23212c5e6ea0
Wesley <wesley_ms14@hotmail.com>
2018-03-25 14:08:09 -0300

    comitando

---
+++ contador.s
@@ -0,0 +1,25 @@
+#######################################################################################
+#Contador e operação xor
+#######################################################################################			
+
+main:		
+		#Atribuições
+		
+		addi r2,r2,20
+		addi r3,r3,20 # ou mov r3,r2 
+		
+		
+########################################################################################
+#Forma 1 de fazer loop e condicoes
+
+repetir:	bne r2, r0, decrementa 		#Laço que se repete até o r2 ser igual a zero  	
+		ret #Finaliza o loop
+		
+decrementa:	subi r2,r2,1
+		call repetir #chama o laço repetir
+#########################################################################################			
+#Forma 2
+
+repetir2:	addi r4,r4,1			#Laço que se repete até o r4 ser igual ao r3	
+		bne r4,r3,repetir2 #if(r4!=r3){//continua no loop}	
+		
